2fc96cf4dd71e2ae39de0e31fc28f1445230acaa
sally <null>
2023-10-04 20:50:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/clas999lesson11/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.clas999lesson11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "AрkjkkSasjdf", Toast.LENGTH_LONG).show()
+    }
+}
---
+++ app/src/main/java/com/example/clas999lesson11/Test.kt
@@ -0,0 +1,4 @@
+package com.example.clas999lesson11
+
+class Test {
+}
