92b0024cbb35fff96e22b16ce0e8b26f708dcd93
sally <null>
2023-10-10 08:47:11 +0900

    show feature two

--- app/src/main/java/com/example/clas999lesson11/MainActivity.kt
+++ app/src/main/java/com/example/clas999lesson11/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Feature second", Toast.LENGTH_LONG).show()
         Toast.makeText(this, "Feature two", Toast.LENGTH_LONG).show()
     }
 }
